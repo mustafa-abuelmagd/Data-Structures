33cb5c8cb9ee825d2f2eb618ca03a51e9bb25ce5
mustafaabuelmagd <mustafa.2buelmagd@gmail.com>
2023-06-24 18:06:11 +0300

    adding the improved search method

--- main.cpp
+++ main.cpp
@@ -75,29 +75,98 @@ public:
 
 };
 
+class LinkedList {
+public:
+    Node *head;
+    Node *tail;
+    int length;
 
+
+    LinkedList() {}
+
+    LinkedList(int headVal) {
+        Node *temp_head = new Node(headVal);
+        head = temp_head;
+        tail = temp_head;
+        length++;
+    }
+
+    Node *getHead() {
+        return head;
+    }
+
+    void insert_node(int val) {
+        Node *temp_head = new Node(val);
+        if (head == nullptr) {
+            head = temp_head;
+            tail = temp_head;
+        } else {
+            tail->next = temp_head;
+            tail = temp_head;
+        }
+        length++;
+    }
+
+    Node *get_nth(int idx) {
+        int itr = 0;
+        for (Node *curr = head; curr; curr = curr->next) {
+            itr++;
+            if (itr == idx) {
+                return curr;
+            }
+        }
+        return nullptr;
+    }
+
+    int indexOf(int val) {
+        int itr = 0;
+        for (Node *temp = head; temp; temp = temp->next, itr++) {
+            if (temp->value == val)
+                return itr;
+        }
+        return -1;
+    }
+
+    int improvedSearch(int val) {
+        int itr = 0;
+        Node *prev = nullptr;
+
+        for (Node *temp = head; temp; temp = temp->next, itr++) {
+            if (temp->value == val) {
+                if (!prev)
+                    return itr;
+                swap(prev->value, temp->value);
+                return itr-1;
+            }
+            prev = temp;
+        }
+        return -1;
+    }
+
+    void print() {
+        for (Node *temp = head; temp; temp = temp->next) {
+            cout << "node " << temp->value << endl;
+        }
+    }
+
+};
 
 int main() {
 
-    Node *head = new Node(1);
-    Node *second = new Node(2);
-    Node *third = new Node(3);
-    Node *fourth = new Node(4);
-    Node *tails = new Node(5);
-
-    head->setNext(second);
-    second->setNext(third);
-    third->setNext(fourth);
-    fourth->setNext(tails);
-    tails->setNext(nullptr);
-
-    tails->setPrevious(fourth);
-    fourth->setPrevious(third);
-    third->setPrevious(second);
-    second->setPrevious(head);
-
-    Node::printFor(head);
-
+    LinkedList *firstLinkedList = new LinkedList();
+    firstLinkedList->insert_node(1);
+    firstLinkedList->insert_node(2);
+    firstLinkedList->insert_node(3);
+    firstLinkedList->insert_node(4);
+    firstLinkedList->insert_node(5);
+    firstLinkedList->insert_node(6);
+    firstLinkedList->insert_node(7);
+    firstLinkedList->insert_node(8);
+    cout << " gotten value " << firstLinkedList->improvedSearch(1) << endl;
+    cout << " gotten value " << firstLinkedList->improvedSearch(6) << endl;
+    cout << " gotten value " << firstLinkedList->improvedSearch(7) << endl;
+    cout << " gotten value " << firstLinkedList->improvedSearch(8) << endl;
+    cout << " gotten value " << firstLinkedList->improvedSearch(6) << endl;
 
     return 0;
 }
